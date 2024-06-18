95fd09d63c775329b37d865d2da91b2d5e3037c2
Yordan Miladinov <yordanm@pm.me>
2024-06-18 10:45:23 +0300

    wip

--- src/components/Web3Provider/wagmi.ts
+++ src/components/Web3Provider/wagmi.ts
@@ -1,47 +1,38 @@
 import { QueryClient } from "@tanstack/react-query";
-import { coinbaseWallet, metaMask, walletConnect } from "wagmi/connectors";
+import { coinbaseWallet, walletConnect, metaMask } from "wagmi/connectors";
 import { createClient } from "viem";
-import { Connector, CreateConnectorFn, createConfig, http } from "wagmi";
+import { CreateConnectorFn, createConfig, http } from "wagmi";
+
+import { Chain } from "viem";
 
 import * as chains from "../../constants/chains";
 import { PARAMS } from "./walletConnect";
 import { injectedWithFallback } from "./injectedWithFallback";
 
 declare module 'wagmi' {
-  interface Register {
-    config: typeof wagmiConfig
-  }
+    interface Register {
+        config: typeof wagmiConfig;
+    }
 }
 
-const inj = injectedWithFallback();
-console.log("inj:", inj);
-inj.name
-
 function makeConfig() {
-    const connectors: CreateConnectorFn[] = [];
-    const pool: CreateConnectorFn[] = [
+    const connectors: CreateConnectorFn[] = [
         injectedWithFallback(),
-        metaMask({
-            preferDesktop: true,
-        }),
+        // metaMask({
+        //     preferDesktop: true,
+        // }),
         coinbaseWallet({
             appName: 'CharmETH',
             reloadOnDisconnect: false,
         }),
         walletConnect(PARAMS),
     ];
-    pool.map((connector: CreateConnectorFn) => {
-        // TODO
-        if (true || !connectors.some(x => x.name == connector.name)) {
-            console.log("PUSHING", connector.name, connector);
-            connectors.push(connector);
-        }
-    });
 
     return createConfig({
         chains: [chains.MAINNET, chains.HOLESKY],
         connectors,
-        client({ chain }) {
+        multiInjectedProviderDiscovery: false,
+        client({ chain }: { chain: Chain<undefined>; }) {
             return createClient({
                 chain,
                 batch: { multicall: true },
